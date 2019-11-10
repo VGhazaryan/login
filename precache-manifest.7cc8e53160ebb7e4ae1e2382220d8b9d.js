a11297361afe7047fb07b49f2461251b43c23375
VGhazaryan <49144300+VGhazaryan@users.noreply.github.com>
2019-11-10 23:28:36 +0400

    'v1.0'

---
+++ precache-manifest.7cc8e53160ebb7e4ae1e2382220d8b9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b91feebd3191db22a40111266a90e62b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd79a9e3b08c58acf414",
+    "url": "/static/css/main.97c3c783.chunk.css"
+  },
+  {
+    "revision": "8821cc64047e6e746130",
+    "url": "/static/js/2.be04f4c9.chunk.js"
+  },
+  {
+    "revision": "bd79a9e3b08c58acf414",
+    "url": "/static/js/main.d9afab0f.chunk.js"
+  },
+  {
+    "revision": "40c747857fe0bc266df9",
+    "url": "/static/js/runtime-main.b5f0a657.js"
+  }
+]);
